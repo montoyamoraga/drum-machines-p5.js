0c07d38f8ab0ab9b3854d0a1df2ea554eb5685f3
montoyamoraga <montoyamoraga@gmail.com>
2019-11-28 16:11:19 -0500

    update draft of gui

--- js/sketch.js
+++ js/sketch.js
@@ -7,19 +7,40 @@ let tempo;
 let stepCurrent;
 let stepNumber = 16;
 
-// drawing variables
-let levelNumbers = 12;
-let levelDiameter = 12;
+// drawing variables and constants
+const levelNumbers = 12;
+const levelDiameter = 12;
+const widthLarge = 1200;
+const heightLarge = 720;
+const sizeFactor = 1.5;
+const widthSmall = widthLarge / sizeFactor;
+const heightSmall = heightLarge / sizeFactor;
 
+// color constants
+let colorStepRed;
+let colorStepOrange;
+let colorStepYellow;
+let colorStepWhite;
+let colorTextRed;
+let colorTextBlack;
+let colorPanelGray;
 
 function setup() {
 
-    createCanvas(800, 400);
+    createCanvas(widthLarge, heightLarge);
 
+    // initial settings
     rectMode(CORNER);
     textAlign(CENTER);
 
-    // black square
+    // define colors
+    colorStepRed = color(255, 0, 0);
+    colorStepOrange = color(255, 255 / 2, 0);
+    colorStepYellow = color(255, 255, 0);
+    colorStepWhite = color(255, 255, 255);
+    colorPanelGray = color(130, 130, 130);
+
+    // black square for instrument
     fill(0);
     rect(0, 0, width, height);
 
@@ -37,22 +58,35 @@ function setup() {
     // sequencer
     for (let i = 0; i < stepNumber; i++) {
         if (i < 4) {
-            fill(255, 0, 0);
+            fill(colorStepRed);
         } else if (i < 8) {
-            fill(255, 255, 0);
+            fill(colorStepOrange);
         } else if (i < 12) {
-            fill(0, 255, 0);
+            fill(colorStepYellow);
         } else {
-            fill(255, 0, 255);
+            fill(colorStepWhite);
         }
         rect(10 * width / 100 + 5 * i * width / 100, 80 * height / 100, 10, 20);
     }
 
+    push();
+    fill(colorPanelGray);
+    stroke(colorPanelGray)
+    strokeWeight(4);
+    // upper gray line
+    line(5 * width / 100, 15 * height / 100, 95 * width / 100, 15 * height / 100);
+    // lower gray line
+    line(5 * width / 100, 66 * height / 100, 95 * width / 100, 66 * height / 100);
+    // 
+    strokeWeight(1);
+    line(30 * width / 100, 15 * height / 100, 30 * width / 100, 66 * height / 100);
+    line(95 * width / 100, 15 * height / 100, 95 * width / 100, 66 * height / 100);
+    pop();
 
     // "Roland" - red letters
     fill(255, 0, 0);
     textSize(20);
-    text("Roland", 70 * width / 100, 10 * height / 100);
+    text("Roland", 70 * width / 100, 8 * height / 100);
 
     // "Rhythm Composer" - red letters
     fill(255, 0, 0);
@@ -65,17 +99,13 @@ function setup() {
     text("Computer Controlled", 77 * width / 100, 60 * height / 100);
 
 
+
 }
 
 function draw() {
 
 
-
-
-
     // sequencer buttons;
-
-
 
 
 }
